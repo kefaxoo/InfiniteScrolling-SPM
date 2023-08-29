7a8093a36684aab7b1e062e31026b008f4cf0c97
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2023-08-29 23:37:59 +0300

    Fix types

--- Sources/InfiniteScrolling-SPM/InfiniteScrollingBehaviourDelegate.swift
+++ Sources/InfiniteScrolling-SPM/InfiniteScrollingBehaviourDelegate.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 public protocol InfiniteScrollingBehaviourDelegate: AnyObject {
-    func configuredCell(forItemAt indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScollingData, for behaviour: InfiniteScrollingBehaviour) -> UICollectionViewCell
-    func didSelectItem(at indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScollingData, for behaviour: InfiniteScrollingBehaviour)
+    func configuredCell(forItemAt indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScrollingData, for behaviour: InfiniteScrollingBehaviour) -> UICollectionViewCell
+    func didSelectItem(at indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScrollingData, for behaviour: InfiniteScrollingBehaviour)
     func didEndScrolling(in behaviour: InfiniteScrollingBehaviour)
     func verticalPaddingForHorizontal(behaviour: InfiniteScrollingBehaviour) -> CGFloat
     func horizontalPaggingForHorizontal(behaviour: InfiniteScrollingBehaviour) -> CGFloat
 }
 
 public extension InfiniteScrollingBehaviourDelegate {
-    func didSelectItem(at indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScollingData, for behaviour: InfiniteScrollingBehaviour) {}
+    func didSelectItem(at indexPath: IndexPath, originallyAt index: Int, and data: InfiniteScrollingData, for behaviour: InfiniteScrollingBehaviour) {}
     func didEndScrolling(in behaviour: InfiniteScrollingBehaviour) {}
     func verticalPaddingForHorizontal(behaviour: InfiniteScrollingBehaviour) -> CGFloat {
         return 0
