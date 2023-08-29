cc7e70ebf48b34a160b41898c53e185d3c2f53f8
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2023-08-29 23:30:34 +0300

    Fix init of CollectionViewConfiguration

--- Sources/InfiniteScrolling-SPM/CollectionViewConfiguration.swift
+++ Sources/InfiniteScrolling-SPM/CollectionViewConfiguration.swift
@@ -11,4 +11,9 @@ public struct CollectionViewConfiguration {
     public let scrollingDirection: UICollectionView.ScrollDirection
     public var layoutType: LayoutType
     public static let shared = CollectionViewConfiguration(scrollingDirection: .horizontal, layoutType: .numebrOfCellOnScreen(5))
+    
+    public init(scrollingDirection: UICollectionView.ScrollDirection, layoutType: LayoutType) {
+        self.scrollingDirection = scrollingDirection
+        self.layoutType = layoutType
+    }
 }
